6092593a05fa19b2e065545c6f00a0f3347b039d
SageMachina2 <nguyen.a225214@stu.sanjac.edu>
2026-09-13 19:04:03 +0000

    Completed BloxburgGame.java

--- BloxburgGame.java
+++ BloxburgGame.java
@@ -1,1 +1,41 @@
+public class BloxburgGame {
+    public static void main(String[] args) {
 
+        System.out.println("Welcome to Bloxburg Busines Simulator!");
+        System.out.println();
+
+        Business pizzaPlace = new Business("Pizza Palace", "Alex", 5000);
+        Business burgerPlace = new Business("Blox Burger", "Nicholas Stewart", 4000);
+
+        pizzaPlace.hireEmployee("Joe Bartolozzi", "Cashier", 13);
+        pizzaPlace.hireEmployee("Case", "Cook", 15);
+
+        burgerPlace.hireEmployee("Jeremy Elbertson", "Cashier", 12);
+        burgerPlace.hireEmployee("Jeremy Webber", "Cook", 14);
+
+        System.out.println(
+            "Total businesses created: "
+            + Business.getTotalBusinesses()
+        );
+
+        System.out.println();
+
+        System.out.println("Business: " + pizzaPlace.getName());
+        System.out.println("Owner: " + pizzaPlace.getOwner());
+
+        System.out.println(
+            "Funds before taxes: $"
+            + pizzaPlace.getFunds()
+        );
+ 
+        pizzaPlace.payTaxes();
+
+        System.out.println(
+            "Funds after taxes: $"
+            + pizzaPlace.getFunds()
+        );
+
+        System.out.println();
+
+        pizzaPlace.displayEmployees();
+}
--- Business.java
+++ Business.java
@@ -1,1 +1,87 @@
+import java.util.ArrayList;
 
+class Business {
+    private String name;
+    private String owner;
+    private double funds;
+    private static int totalBusinesses = 0;
+    public static final double TAX_RATE = 0.1;
+
+    private ArrayList<Employee> employees;
+
+    public Business(String name) {
+        this(name, "N/a", 1000);
+    }
+
+    public Business(String name, double startingFunds) {
+        this(name, "N/a", startingFunds);
+    }
+ 
+    public Business(String name, String owner, double startingFunds) {
+        this.name = name;
+        this.owner = owner;
+        this.funds = startingFunds;
+
+        employees = new ArrayList<Employee>();
+
+        totalBusinesses++;
+    }
+ 
+    public void payTaxes() {
+        funds -= funds * TAX_RATE;
+    }
+
+    public void hireEmployee(String name, String role, double salary) {
+        Employee newEmployee = new Employee(name, role, salary);
+        employees.add(newEmployee);
+    }
+
+    public void paySalaries(double hoursWorked) {
+        for (Employee employee : employees) {
+            double pay = employee.salary * hoursWorked;
+            funds -= pay;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getOwner() {
+        return owner;
+    }
+
+    public double getFunds() {
+        return funds;
+    }
+
+    public static int getTotalBusinesses() {
+        return totalBusinesses;
+    }
+
+    public void displayEmployees() {
+        for (Employee employee : employees) {
+            employee.employeeInfo();
+        }
+    }
+ 
+    class Employee {
+        private String name;
+        private String role;
+        private double salary;
+ 
+        public Employee(String name, String role, double salary) {
+            this.name = name;
+            this.role = role;
+            this.salary = salary;
+        }
+
+        public void employeeInfo() {
+            System.out.println(
+                "Employee: " + name +
+                " | Role: " + role +
+                " | Salary: $" + salary + "/hour"
+            );
+        }
+    }
+}
